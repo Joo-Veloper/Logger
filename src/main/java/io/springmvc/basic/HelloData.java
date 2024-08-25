9f6a5c329011f8b29d1e6eb1e0e3be90c11c7406
Seung Joo <psjoo3515@gmail.com>
2024-08-25 18:56:29 +0900

    @ModelAttribute

---
+++ src/main/java/io/springmvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package io.springmvc.basic;
+
+import lombok.Data;
+/**
+ * @Data
+ * `@Getter`, `@Setter`, `@ToString`, `@EqualsAndHashCode`, `@RequiredArgsConstructor`
+ * */
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
