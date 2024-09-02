c549f756b61179a6f9cd752ff43bad70a104466a
Seung Joo <psjoo3515@gmail.com>
2024-09-02 17:52:59 +0900

    Redirect Attributes

--- src/main/java/io/springmvc/web/domain/controller/BasicItemController.java
+++ src/main/java/io/springmvc/web/domain/controller/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -67,7 +68,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    /*@PostMapping("/add")*/
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
@@ -76,9 +77,17 @@ public class BasicItemController {
     /**
      * PRG - Post/Redirect/Get
      */
-    @PostMapping("/add")
+    /*@PostMapping("/add")*/
     public String addItemV5(Item item) {
         itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getItemName());
+        redirectAttributes.addAttribute("status", true);
         return "redirect:/basic/items/" + item.getId();
     }
 
