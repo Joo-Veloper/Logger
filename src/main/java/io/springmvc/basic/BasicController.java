0ae760867ada55660691bae04f73f73ceb76d878
Seung Joo <psjoo3515@gmail.com>
2024-09-03 15:12:11 +0900

    변수 - SpringEL

--- src/main/java/io/springmvc/basic/BasicController.java
+++ src/main/java/io/springmvc/basic/BasicController.java
@@ -1,23 +1,59 @@
 package io.springmvc.basic;
 
+import lombok.Data;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
     @GetMapping("/text-basic")
     public String textBasic(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-basic";
         /** HTML 엔티티
          * 웹 브라우저는 `<` 를 HTML 테그의 시작으로 인식한다. 따라서 `<`를 태그의 시작이 아닌 문자로 표현할 수 있는 방법이 필요한데 이것을 HTML 엔티티 라고 한다.
          * HTML에서 사용하는 특수 문자는 HTML 엔티티로 변경하는 것을 이스케이프라고 하며 타입리프가 제공하는, `th:test`, [[...]] 는 기본적 Escape를 제공 */
-        model.addAttribute("data", "Hello <b>Spring!</b>");
-        return "basic/text-basic";
     }
+
     @GetMapping("/text-unescaped")
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
