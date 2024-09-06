df4f51ee4c4b78fab2090fb870ad9d574e71fd1e
Seung Joo <psjoo3515@gmail.com>
2024-09-06 20:21:47 +0900

    @Bean -> MessageSource

--- src/main/java/io/springmvc/SpringmvcApplication.java
+++ src/main/java/io/springmvc/SpringmvcApplication.java
@@ -2,6 +2,9 @@ package io.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class SpringmvcApplication {
@@ -10,4 +13,11 @@ public class SpringmvcApplication {
         SpringApplication.run(SpringmvcApplication.class, args);
     }
 
+	/*@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}*/
 }
