ddb519bda716ede1db75da29cec90dad5830229a
Seung Joo <psjoo3515@gmail.com>
2024-09-11 15:23:26 +0900

    using Validation

--- src/main/java/io/springmvc/web/domain/item/entity/Item.java
+++ src/main/java/io/springmvc/web/domain/item/entity/Item.java
@@ -1,7 +1,11 @@
 package io.springmvc.web.domain.item.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
@@ -9,14 +13,22 @@ import java.util.List;
 @NoArgsConstructor
 public class Item {
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
+
     private Boolean open; //판매 여부
     private List<String> regions; // 등록 지연
     private ItemType itemType; //상품 종류
     private String deliveryCode;// 배송방식
-
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
