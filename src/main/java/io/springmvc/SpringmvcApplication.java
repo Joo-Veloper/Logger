182e5f1b5c22000b5040d3f07b8ee6f16e7baea6
Seung Joo <psjoo3515@gmail.com>
2024-09-11 14:54:17 +0900

    ItemValidator Separation

--- src/main/java/io/springmvc/SpringmvcApplication.java
+++ src/main/java/io/springmvc/SpringmvcApplication.java
@@ -1,17 +1,26 @@
 package io.springmvc;
 
+import io.springmvc.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringmvcApplication {
+public class SpringmvcApplication /*implements WebMvcConfigurer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcApplication.class, args);
     }
+
+    /*@Override
+    public Validator getValidator() {
+        return new ItemValidator();
+    }*/
 
 	/*@Bean
 	public MessageSource messageSource() {
