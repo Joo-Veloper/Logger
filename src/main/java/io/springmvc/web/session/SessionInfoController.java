d985c0fd2d8e79b2d899397803861561a4d3246c
Seung Joo <psjoo3515@gmail.com>
2024-09-14 16:58:36 +0900

    Session TimeOut Setting Test

---
+++ src/main/java/io/springmvc/web/session/SessionInfoController.java
@@ -0,0 +1,32 @@
+package io.springmvc.web.session;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "세션이 없습니다.";
+        }
+        // 세션 데이터 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> SessionInfoController.log.info("session name={}, value={}", name, session.getAttribute(name)));
+
+        log.info("sessionId={}", session.getId());// 세션 ID
+        log.info("getMaxInactiveInterval={}", session.getMaxInactiveInterval()); // 세션 유효시간 ex) 1800초(30분)
+        log.info("creationTime={}", new Date(session.getCreationTime())); // 세션 생성일자
+        log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime())); // 세션과 연결된 사용자의 마지막 접근 시간
+        log.info("isNew={}", session.isNew()); // 새로 생성된 세션 인지 확인
+
+        return "세션 출력";
+    }
+}
