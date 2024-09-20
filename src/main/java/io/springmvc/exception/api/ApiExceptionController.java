2d7b17334f8c5fd821f742da50ff789f0f19ddc4
Seung Joo <psjoo3515@gmail.com>
2024-09-20 13:49:21 +0900

    ExceptionResolver provided by Spring

--- src/main/java/io/springmvc/exception/api/ApiExceptionController.java
+++ src/main/java/io/springmvc/exception/api/ApiExceptionController.java
@@ -1,11 +1,14 @@
 package io.springmvc.exception.api;
 
+import io.springmvc.exception.exception.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -25,9 +28,19 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello " + id);
     }
 
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
     @Data
     @AllArgsConstructor
-    static class MemberDto{
+    static class MemberDto {
         private String memberId;
         private String name;
     }
