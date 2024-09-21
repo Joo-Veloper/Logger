5d0593579cbf4d8d21c0f1479d07187c8ce13d35
Seung Joo <psjoo3515@gmail.com>
2024-09-21 20:34:39 +0900

    ConverterTest

---
+++ src/test/java/io/springmvc/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,41 @@
+package io.springmvc.typeconverter.converter;
+
+import io.springmvc.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConverterTest {
+    @Test
+    void StringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void IntegerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void StringToIpPort() {
+        IpPortToStringConvert converter = new IpPortToStringConvert();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortTestString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+
+}
