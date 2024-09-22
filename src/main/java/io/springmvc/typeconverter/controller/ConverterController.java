dd4de952cf27e4b656e27cc424c10552498b18b3
Seung Joo <psjoo3515@gmail.com>
2024-09-22 16:29:59 +0900

    Apply View Template Converters

---
+++ src/main/java/io/springmvc/typeconverter/controller/ConverterController.java
@@ -0,0 +1,45 @@
+package io.springmvc.typeconverter.controller;
+
+import io.springmvc.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter/converter-view";
+    }
+
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080); // IpPort 객체 생성 후
+        Form form = new Form(ipPort);                          // Form 에 집어 넣음
+        model.addAttribute("form", form);          // Form에 IpPort 담아서 넘김
+        return "converter/converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter/converter-view";
+    }
+
+    @Data
+    static class Form {
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
+
+}
